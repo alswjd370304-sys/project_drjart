5c4c3f4f8f353cbf627e42271559a4202852cec9
alswjd370304-sys <alswjd370304@gamil.com>
2026-09-11 22:14:07 +0900

    1행 반응형 진행 중

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
 //index.js
 //swiper 활성화
-const heroSLide = new Swiper('.hero',{})
+const heroSLide = new Swiper('.hero',{
+    loop: true,
+    direction:'horizontal',
+    autoplay:{
+        delay:4000,
+        disableOnInteraction:false,
+    },
+    navigation: {
+        prevEl: '.image_banner_prev',
+        nextEl: '.image_banner_next',
+    },
+    pagination:{
+        el: '.image_banner_page',
+    }
+})
