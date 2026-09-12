f09a34575757189601fcab99eb9a9d332f470958
alswjd370304-sys <alswjd370304@gamil.com>
2026-09-12 18:25:58 +0900

    2행 자바스크립트+반응형 완성

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const heroSLide = new Swiper('.hero',{
     loop: true,
     direction:'horizontal',
     autoplay:{
-        delay:4000,
+        delay:6000,
         disableOnInteraction:false,
     },
     navigation: {
@@ -13,5 +13,17 @@ const heroSLide = new Swiper('.hero',{
     },
     pagination:{
         el: '.image_banner_page',
+    }
+})
+
+//해당 스크롤에 도착 시 딱 한 번 애니메이션을 진행하고 새로고침 전까지 작동 안함.
+//브랜드 슬로건 영역에 도착시 브랜드 슬로건 애니메이션 실행
+let animation = false; // 실행안함
+const brand_slogan = document.querySelector('.brand_slogan');
+//
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 200 &&  animation == false){
+        brand_slogan.classList.add('active');
+        animation = true;
     }
 })
