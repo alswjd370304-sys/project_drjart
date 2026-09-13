0d77e2e312772eb2a8b0eafeedc51beaff138707
alswjd370304-sys <alswjd370304@gamil.com>
2026-09-13 16:56:41 +0900

    3행 이미지 연결 및 호버 진행 중

--- scripts/index.js
+++ scripts/index.js
@@ -16,10 +16,17 @@ const heroSLide = new Swiper('.hero',{
     }
 })
 
+//변수
+//1행 변수
+const brand_slogan = document.querySelector('.brand_slogan');
+//3행 변수
+const swiperWrapper = document.querySelector('.best_product_swiper .swiper-wrapper'); //붙여넣기할 대상
+
+
+//------------------------------------------------------------------------------1행 swiper
 //해당 스크롤에 도착 시 딱 한 번 애니메이션을 진행하고 새로고침 전까지 작동 안함.
 //브랜드 슬로건 영역에 도착시 브랜드 슬로건 애니메이션 실행
 let animation = false; // 실행안함
-const brand_slogan = document.querySelector('.brand_slogan');
 //
 window.addEventListener('scroll', ()=>{
     if(scrollY > 10 &&  animation == false){
@@ -27,3 +34,24 @@ window.addEventListener('scroll', ()=>{
         animation = true;
     }
 })
+
+//------------------------------------------------------------------------------2행 swiper
+const bestSlide = new Swiper('.best_product_swiper',{
+    slidesPerView:4,
+    spaceBetween:19,
+}) //swiper 실행
+
+for(let i=0; i<=5; i++){
+    const bestSlideDiv = document.createElement('div');
+    bestSlideDiv.classList.add('swiper-slide');
+
+    bestSlideDiv.innerHTML = `<a href="#" class="best_product_img${bestDB[i].id}"></a>`
+    bestSlideDiv.innerHTML += `<div class="product_name">
+    <p class="name">${bestDB[i].name}</p>
+    <p class="sub_txt">${bestDB[i].sub_txt}</p>
+    </div>`
+    bestSlideDiv.innerHTML += `<p class="price">${bestDB[i].price}</p>`
+    bestSlideDiv.innerHTML += `<p class="cart"><button type="button" class="cart_btn">장바구니</button></p>`
+
+    swiperWrapper.appendChild(bestSlideDiv); //붙여넣기
+}
