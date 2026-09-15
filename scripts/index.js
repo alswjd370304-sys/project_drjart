c735620dced79fd09039a95646c2e2fc582f1c4d
alswjd370304-sys <alswjd370304@gmail.com>
2026-09-15 18:08:41 +0900

    메뉴창 완성

--- scripts/index.js
+++ scripts/index.js
@@ -16,11 +16,18 @@ const heroSLide = new Swiper('.hero',{
     }
 })
 
-//변수
+//변수정리
 //1행 변수
 const brand_slogan = document.querySelector('.brand_slogan');
 //3행 변수
 const swiperWrapper = document.querySelector('.best_product_swiper .swiper-wrapper'); //붙여넣기할 대상
+/* 메뉴 변수 */
+const totalMenu = document.querySelector('#total_menu'); //전체메뉴부모
+const menuOverlay = document.querySelector('.menu_overlay'); //메뉴창
+const closeBtn = document.querySelector('.close_btn'); //닫기버튼
+/* 메뉴탭 변수 */
+const menuTab = document.querySelectorAll('.menu_tab');
+const menuContent = document.querySelectorAll('.menu_content');
 
 
 //------------------------------------------------------------------------------2행 swiper
@@ -111,3 +118,40 @@ const skinSwiper = new Swiper('.skin_solution_swiper', {
         el: '.skin_page',
     }
 })
+
+//------------------------------------------------------------------------------메뉴 활성화
+/* 전체메뉴 눌렀을 때 메뉴창 열기 - 닫기 버튼을 눌렀을 때 닫히기 */
+
+//전체메뉴를 눌렀을 때 열리기 (active 클래스 넣기)
+totalMenu.addEventListener('click', ()=>{
+    menuOverlay.classList.add('active');
+})
+//닫기 버튼 눌렀을 때 메뉴 닫히기 (클래스 빼기)
+closeBtn.addEventListener('click', ()=>{
+    menuOverlay.classList.remove('active')
+})
+//메뉴 오버레이를 클릭 했을 때 메뉴 닫히기 (클래스 빼기)
+menuOverlay.addEventListener('click', (e)=>{
+    if(e.target === menuOverlay){
+        menuOverlay.classList.remove('active');
+    }
+})
+
+//탭 메뉴를 눌렀을 때 해당 메뉴의 내용 보이도록 하기
+menuTab[0].addEventListener('click', ()=>{
+    menuTab[0].classList.add('active');
+    menuTab[2].classList.remove('active');
+
+    menuContent[0].classList.add('active');
+    menuContent[1].classList.remove('active');
+});
+
+menuTab[2].addEventListener('click', function() {
+
+    menuTab[0].classList.remove('active');
+    menuTab[2].classList.add('active');
+
+    menuContent[0].classList.remove('active');
+    menuContent[1].classList.add('active');
+
+});
